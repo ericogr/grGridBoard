bc62540f0c41b894f10eb664e33c63300a947542
EricoGR <ericomercial@yahoo.com.br>
2015-07-16 11:11:24 -0300

    first functional implementation of grid board

--- src/gr-grid-board.js
+++ src/gr-grid-board.js
@@ -6,21 +6,27 @@ angular.module('grGridBoard', [])
 			scope: {
 				lines: '=',
 				columns: '=',
-				width: '=',
-				height: '='
+				width: '@',
+				height: '@',
+				lineColour: '@'
 			},
 			controller: function($scope, $element) {
 				$scope.viewBox = "0 0 " + $scope.width + " " + $scope.height;
 				$scope.rectWidth = $scope.width / $scope.columns;
 				$scope.rectHeight = $scope.height / $scope.lines;
 				$scope.positions = generatePositions($scope.lines, $scope.columns, $scope.rectWidth, $scope.rectHeight);
+				$scope.lineColour = $scope.lineColour || 'black';
+
+				$scope.clickRect = function(posRef) {
+					posRef.selected = !posRef.selected;
+				};
 
 				function generatePositions(lines, columns, width, height) {
 					var ret = [];
 
 					for (var l = 0; l < lines; l++) {
 						for (var c = 0; c < columns; c++) {
-							ret.push({"x": c * width, "y": l * height});
+							ret.push({"line": l, "column": c, "x": c * width, "y": l * height, "selected": false});
 						}
 					}
 
