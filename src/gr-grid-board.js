3360f83888e38d094d66ca4f3316823f083e5128
erico <ericomercial@yahoo.com.br>
2015-07-15 21:20:24 -0300

    desenv

--- src/gr-grid-board.js
+++ src/gr-grid-board.js
@@ -1,12 +1,28 @@
 angular.module('grGridBoard', [])
 	.directive('grGridBoard', function() {
 	    return {
-	      restrict: 'E',
-	      transclude: true,
-	      scope: {},
-	      controller: function($scope, $element) {
-	      },
-	      templateUrl: 'svg-grid-board.html',
-	      replace: true
+			restrict: 'E',
+			transclude: true,
+			scope: {
+				lines: '@',
+				columns: '@'
+			},
+			controller: function($scope, $element) {
+				$scope.positions = generatePositions(8, 8);
+
+				function generatePositions(lines, columns) {
+					var ret = [];
+
+					for (var l = 0; l < lines; l++) {
+						for (var c = 0; c < columns; c++) {
+							ret.push({"x": l, "y": c});
+						}
+					}
+
+					return ret;
+				}
+			},
+			templateUrl: 'svg-grid-board.html',
+			replace: true
 	    };
-	  });
+	});
