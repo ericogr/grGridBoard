36e7a183137abcf0b109d4e9814fd5b603e2abef
erico <ericomercial@yahoo.com.br>
2015-07-15 23:31:26 -0300

    add attrs to component

--- src/gr-grid-board.js
+++ src/gr-grid-board.js
@@ -4,18 +4,23 @@ angular.module('grGridBoard', [])
 			restrict: 'E',
 			transclude: true,
 			scope: {
-				lines: '@',
-				columns: '@'
+				lines: '=',
+				columns: '=',
+				width: '=',
+				height: '='
 			},
 			controller: function($scope, $element) {
-				$scope.positions = generatePositions(8, 8);
+				$scope.viewBox = "0 0 " + $scope.width + " " + $scope.height;
+				$scope.rectWidth = $scope.width / $scope.columns;
+				$scope.rectHeight = $scope.height / $scope.lines;
+				$scope.positions = generatePositions($scope.lines, $scope.columns, $scope.rectWidth, $scope.rectHeight);
 
-				function generatePositions(lines, columns) {
+				function generatePositions(lines, columns, width, height) {
 					var ret = [];
 
 					for (var l = 0; l < lines; l++) {
 						for (var c = 0; c < columns; c++) {
-							ret.push({"x": l, "y": c});
+							ret.push({"x": c * width, "y": l * height});
 						}
 					}
 
