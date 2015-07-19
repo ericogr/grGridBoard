ccb686c7d0b4e427a5f79ed77c6b4fe06cfa3299
erico <ericomercial@yahoo.com.br>
2015-07-18 23:46:23 -0300

    measures the time each task takes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,9 @@
 'use strict';
 
 module.exports = function(grunt) {
-    require('load-grunt-config') (grunt);
+    // measures the time each task takes
+    require('time-grunt')(grunt);
+
+    // load grunt config
+    require('load-grunt-config')(grunt);
 };
