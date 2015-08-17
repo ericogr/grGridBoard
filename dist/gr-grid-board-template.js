ab1bc9bee542a0f081a79286e704fa2b6a0b4feb
erico <ericomercial@yahoo.com.br>
2015-08-17 19:48:03 -0300

    add class to rect

--- dist/gr-grid-board-template.js
+++ dist/gr-grid-board-template.js
@@ -4,7 +4,9 @@ angular.module('grGridBoard').run(['$templateCache', function($templateCache) {
   $templateCache.put('gr-grid-board-template',
     "<svg xmlns=\"http://www.w3.org/2000/svg\" ng-attr-viewBox=\"{{viewBox}}\">\n" +
     "\t<rect \n" +
-    "\t\tng-repeat=\"pos in positions\" ng-click=\"clickRect(pos.line, pos.column)\"\n" +
+    "\t\tng-repeat=\"pos in positions\"\n" +
+    "\t\tclass=\"gr-grid-rect gr-grid-rect-{{pos.line}}x{{pos.column}}\"\n" +
+    "\t\tng-click=\"clickRect(pos.line, pos.column)\"\n" +
     "\t\tng-attr-x=\"{{pos.x}}\" ng-attr-y=\"{{pos.y}}\"\n" +
     "\t\tng-attr-width=\"{{rectWidth}}\" ng-attr-height=\"{{rectHeight}}\"\n" +
     "\t\tng-attr-stroke=\"{{lineColour}}\" ng-attr-fill=\"{{values[pos.line][pos.column] ? selectedColour : unselectedColour}}\" fill-opacity=\"1.0\" stroke-opacity=\"1.0\">\n" +
