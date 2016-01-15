9d02991d1e24779b8b3e38ac1e6093a292e1c41e
Érico <ericogr@gmail.com>
2016-01-14 23:42:07 -0200

    include width and height attributes to be compatible with firefox

--- dist/gr-grid-board-template.js
+++ dist/gr-grid-board-template.js
@@ -5,6 +5,8 @@ angular.module('grGridBoard').run(['$templateCache', function($templateCache) {
     "<svg xmlns=\"http://www.w3.org/2000/svg\" ng-attr-viewBox=\"{{viewBox}}\">\n" +
     "\t<rect \n" +
     "\t\tng-repeat=\"pos in positions\"\n" +
+    "\t\twidth=\"{{rectWidth}}\"\n" +
+    "\t\theight=\"{{rectHeight}}\"\n" +
     "\t\tclass=\"gr-grid-rect gr-grid-rect-{{pos.line}}x{{pos.column}}\"\n" +
     "\t\tng-class=\"{selected: values[pos.line][pos.column], unselected: !values[pos.line][pos.column]}\"\n" +
     "\t\tng-click=\"clickRect(pos.line, pos.column)\"\n" +
